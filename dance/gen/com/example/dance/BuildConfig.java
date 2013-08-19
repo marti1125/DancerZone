e4635e4ec11c558ef50b31e4a7439f3bb8ad3f90
Willy Aguirre <marti1125@gmail.com>
2013-08-18 20:00:06 -0500

    se agrega el proyecto en android

---
+++ dance/gen/com/example/dance/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.dance;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ dance/src/com/example/dance/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.dance;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import org.apache.cordova.*;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        // Set by <content src="index.html" /> in config.xml
+        super.loadUrl(Config.getStartUrl());
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+}
